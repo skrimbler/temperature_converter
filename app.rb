28337c87f1936bf30f8f1b683fbaedd3784c1c6c
Niels Van Vlaenderen <nielsvanvlaenderen@hotmail.com>
2016-09-26 10:16:05 +0200

    read from textfile

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require './lib/tem_converter.rb'
 
-input = ARGV.first.to_f
+#input = ARGV.first.to_f
+input = File.open('temperature.txt').read.to_f
 
 tem_converter = TemConverter.new input
 puts tem_converter.convert
