ad887d8ab77975fa258d34fcf24214c65ba7c5fd
Niels Van Vlaenderen <nielsvanvlaenderen@hotmail.com>
2016-09-26 10:25:33 +0200

    read from url

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require './lib/tem_converter.rb'
 
+require 'net/http'
+require 'uri'
+
+input =  Net::HTTP.get(URI.parse("http://labict.be/software-engineering/temperature/api/temperature/fake")).to_f
+
+
 #input = ARGV.first.to_f
-input = File.open('temperature.txt').read.to_f
+#input = File.open('temperature.txt').read.to_f
 
 tem_converter = TemConverter.new input
 puts tem_converter.convert
