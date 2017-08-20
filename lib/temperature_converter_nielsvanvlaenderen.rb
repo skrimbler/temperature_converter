b1a516937dc2b56fec67b9182744ed50cd5c79f3
Niels Van Vlaenderen <nielsvanvlaenderen@hotmail.com>
2017-08-20 15:07:00 +0200

    files hernoemen en code van file verplaatsen

--- lib/temperature_converter_nielsvanvlaenderen.rb
+++ lib/temperature_converter_nielsvanvlaenderen.rb
@@ -1,22 +1,11 @@
+require './lib/read_cli.rb'
+require './lib/read_file.rb'
+require './lib/read_mqtt.rb'
+require './lib/read_url.rb'
 
-require './lib/temperature_read_file.rb'
-require './lib/temperature_read_ttl.rb'
-require './lib/temperature_read_url.rb'
-require './lib/temperature_read_commandline.rb'
+require './lib/print_text.rb'
+require './lib/print_json.rb'
+require './lib/print_html.rb'
 
-require './lib/temperature_converter_commandline.rb'
-require './lib/temperature_converter_file.rb'
-require './lib/temperature_converter_url.rb'
-require './lib/temperature_converter_ttl.rb'
 require './lib/temperature_converter.rb'
-
-
-require './lib/temperature_output.rb'
-require './lib/temperature_output_to_html.rb'
-require './lib/temperature_output_to_json.rb'
-require './lib/temperature_output_to_text.rb'
-
-require './lib/temperature_convert.rb'
-require './lib/temperature_convert_to_celcius.rb'
-require './lib/temperature_convert_to_fahrenheit.rb'
-require './lib/temperature_convert_to_kelvin.rb'
+require './lib/temperature.rb'
